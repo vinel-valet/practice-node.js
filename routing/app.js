710fe82f3e08c12e363e78137ef9108fc49ff929
Nikolay <94299978+vinel-valet@users.noreply.github.com>
2023-08-09 17:01:51 +0200

    routing

---
+++ routing/app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    console.log('Server');
+    res.setHeader('Content-type', 'text/html')
+    if (req.url === '/') {
+        fs.readFile('./home.html', (err, data) => {
+            if (err) {
+                console.log(err);
+                res.end();
+            } else {
+                res.end(data)
+            }
+        })
+    }
+});
+
+server.listen(3000, 'localhost', (error) => {
+    error ? console.log(error) : console.log('Listening port')
+})
