521fe7bc7f5607ce247a137a44d20057aefd49b1
Nikolay <94299978+vinel-valet@users.noreply.github.com>
2023-08-10 18:27:16 +0200

    add ejs use post req

--- routing/express-server.js
+++ routing/express-server.js
@@ -1,20 +1,40 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const createPath = (page) => path.join(__dirname, `${page}.html`)
+const PORT = 3000;
+const createPath = (page) => path.join(__dirname, `${page}.html`);
+const listName = [
+    {name: 'Nikolay', id: 1},
+    {name: 'Arina', id: 2},
+    {name: 'Nitita', id: 3},
+]
 
-app.listen(3000, (error) => {
-    error ? console.log(error) : console.log('Listening port')
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('routing/styles'));
+app.use(express.urlencoded({extended: false}));
+
+app.listen(PORT, (error) => {
+    error ? console.log(error) : console.log(`Listening port ${PORT}`)
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(createPath('home'))
+    /*res.sendFile(createPath('home'))*/
+    res.render('list', { listName, active: 'home'})
 });
-app.get('/about', (req, res) => {
+app.post('/add', (req, res) => {
+    const newName = {name: req.body.name, id: new Date()};
+    listName.push(newName);
+    res.redirect('/');
+})
+
+app.get('/features', (req, res) => {
+    res.render('features', {surname: 'Vinel', active: 'features'})
+});
+app.get('/names', (req, res) => {
     res.redirect('/')
 });
 
 app.use((req, res) => {
     res.sendFile(createPath('error'))
 });
-
