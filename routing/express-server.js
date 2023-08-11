2781628aa6a756425166421f666d650d94caa515
Nikolay <94299978+vinel-valet@users.noreply.github.com>
2023-08-11 17:52:41 +0200

    added mongoDB

--- routing/express-server.js
+++ routing/express-server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const User = require('./models/user')
 const PORT = 3000;
+const db = 'mongodb+srv://vinel-admin:mano-9.kolyan@cluster0.vfoic0m.mongodb.net/node-list-name?retryWrites=true&w=majority';
+
+mongoose.connect(db)
+    .then((res)=> console.log('connected'))
+    .catch((error)=> console.log(error));
+
 const createPath = (page) => path.join(__dirname, `${page}.html`);
-const listName = [
-    {name: 'Nikolay', id: 1},
-    {name: 'Arina', id: 2},
-    {name: 'Nitita', id: 3},
-]
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -18,15 +21,17 @@ app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`Listening port ${PORT}`)
 })
 
-app.get('/', (req, res) => {
-    /*res.sendFile(createPath('home'))*/
-    res.render('list', { listName, active: 'home'})
+app.get('/user/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then((user) => res.render('user', { user, active: 'home' }))
+        .catch((error)=> console.log(error))
 });
-app.post('/add', (req, res) => {
-    const newName = {name: req.body.name, id: new Date()};
-    listName.push(newName);
-    res.redirect('/');
-})
+
+app.get('/', (req, res) => {
+    User.find()
+        .then((listName) => res.render('list', { listName, active: 'home'}))
+        .catch((error)=> console.log(error))
+});
 
 app.get('/features', (req, res) => {
     res.render('features', {surname: 'Vinel', active: 'features'})
@@ -34,6 +39,14 @@ app.get('/features', (req, res) => {
 app.get('/names', (req, res) => {
     res.redirect('/')
 });
+
+app.post('/add', (req, res) => {
+    const newName = {name: req.body.name};
+    const user = new User(newName);
+    user.save()
+        .then(()=>res.redirect('/'))
+        .catch((error)=>console.log(error))
+})
 
 app.use((req, res) => {
     res.sendFile(createPath('error'))
